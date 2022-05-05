615126c88feb724adb23f1991296e6e439115f3a
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-05-06 01:38:14 +0400

    Update DemonMusicBot.py

--- DemonMusicBot.py
+++ DemonMusicBot.py
@@ -116,7 +116,7 @@ def a(client, message):
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name, performer="@slmmnhusnu")
+        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name, performer="@thehusnumustafayev)
         m.delete()
         bot.send_audio(chat_id=Config.PLAYLIST_ID, audio=audio_file, caption=rep, performer="@thehusnumustafayevblog", parse_mode='md', title=title, duration=dur, thumb=thumb_name)
     except Exception as e:
